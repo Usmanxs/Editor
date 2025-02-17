8ad6ec99e1d1ae027ea018086e1c3e26413b794f
usmanxs <usman007be@gmail.com>
2025-02-17 15:25:04 +0500

    discard useless

--- lib/pdfjs.ts
+++ lib/pdfjs.ts
@@ -2,7 +2,7 @@ import { GlobalWorkerOptions } from 'pdfjs-dist';
 
 if (typeof window !== 'undefined') {
   // Use a dynamic import for the worker
-  GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${process.env.NEXT_PUBLIC_PDFJS_VERSION}/build/pdf.worker.min.js`;
+
 }
 
 // Export the initialized pdfjs
